dfb31404716b233a31b5b97afa595968b7734c62
Phantomcoder-ai <romkayanchik@ukr.net>
2026-03-14 19:50:14 +0100

    MainMenu(1)

--- EducationalGame/Assets/Scripts/Game/GameManager.cs
+++ EducationalGame/Assets/Scripts/Game/GameManager.cs
@@ -1,6 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager
+public enum GameState
 {
-    
+    Menu,
+    Playing,
+    Paused,
+    Win,
+    GameOver
+}
+public enum GameMode
+{
+    Words,
+    Math
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameState currentState;
+    public GameMode currentMode;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        currentState = GameState.Menu;
+        Debug.Log("Game started in state: " + currentState);
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        currentState = newState;
+
+        Debug.Log("Game State: " + currentState);
+    }
+    public void StartWordMode()
+    {
+        currentMode = GameMode.Words;
+        currentState = GameState.Playing;
+
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void StartMathMode()
+    {
+        currentMode = GameMode.Math;
+        currentState = GameState.Playing;
+        SceneManager.LoadScene("GameScene");
+    }
 }
